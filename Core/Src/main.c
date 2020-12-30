2c572395609d26cfe2de5aa128769cb52f8769b8
Maxwell <maxwell.li@bepmarine.com>
2020-12-30 17:47:56 +1300

    EXTERNAL_LOOPBACK

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -232,7 +232,7 @@ static void MX_FDCAN1_Init(void)
   /* USER CODE END FDCAN1_Init 1 */
   hfdcan1.Instance = FDCAN1;
   hfdcan1.Init.FrameFormat = FDCAN_FRAME_FD_BRS;
-  hfdcan1.Init.Mode = FDCAN_MODE_INTERNAL_LOOPBACK;
+  hfdcan1.Init.Mode = FDCAN_MODE_EXTERNAL_LOOPBACK;
   hfdcan1.Init.AutoRetransmission = DISABLE;
   hfdcan1.Init.TransmitPause = DISABLE;
   hfdcan1.Init.ProtocolException = DISABLE;
