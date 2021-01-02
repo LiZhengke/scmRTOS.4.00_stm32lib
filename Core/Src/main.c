b633632ac6103725c30cb954e0ecde81eb1a85ac
Maxwell <maxwell.li@bepmarine.com>
2021-01-03 12:05:35 +1300

    TIM3 IT

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -298,9 +298,9 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 0;
+  htim3.Init.Prescaler = 1;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 65535;
+  htim3.Init.Period = 10000;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
@@ -325,7 +325,9 @@ static void MX_TIM3_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN TIM3_Init 2 */
-
+	if (HAL_TIM_Base_Start_IT(&htim3) != HAL_OK) {
+		Error_Handler();
+	}
   /* USER CODE END TIM3_Init 2 */
 
 }
@@ -474,6 +476,7 @@ void ATECmdsTask(void *argument)
 	}
   /* USER CODE END 5 */
 }
+
 /* CanTimerFunc function */
 void CanTimerFunc(void *argument)
 {
